6a7a325a5b47adf0e58d42fef59d3340208ae547
Youshida <koshelev.dima.1999@yandex.ru>
2019-08-20 08:27:34 +0300

    first commit

---
+++ Game/Render.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Input;
+using OpenTK.Input;
+
+namespace Game
+{
+    class Render : GameWindow
+    {
+        List<Texture2D> textures;
+        View view;
+        public Render(int width, int height) 
+            : base(width, height)
+        {
+            GL.Enable(EnableCap.Texture2D);
+
+            view = new View(Vector2.Zero, 0.0, 1.0);
+            
+            Input.Initialize(this);
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            textures = new List<Texture2D>();
+            base.OnLoad(e);
+            textures.Add(ContentPipe.LoadTexture("T1/bricks.png"));
+            textures.Add(ContentPipe.LoadTexture("T1/dark_oak_planks.png"));
+            textures.Add(ContentPipe.LoadTexture("T1/dirt.png"));
+            textures.Add(ContentPipe.LoadTexture("T1/grass_block_side.png"));
+        }
+        protected override void OnUpdateFrame(FrameEventArgs e)
+        {
+            base.OnUpdateFrame(e);
+
+            if (Input.MousePress(MouseButton.Left))
+            {
+                var x = Mouse.GetCursorState().X - X;
+                var y = Mouse.GetCursorState().Y - Y;
+                Vector2 pos = new Vector2(x, y) - new Vector2(Width, Height) / 2f;
+                pos = view.ToWorld(pos);
+                view.SetPosition(pos, TweenType.Linear, 15);
+            }
+            if (Input.KeyPress(Key.Up))
+            {
+                view.SetPosition(view.PositionGoTo + new Vector2(0, -20), TweenType.Linear, 15);
+            }
+            if (Input.KeyPress(Key.Down))
+            {
+                view.SetPosition(view.PositionGoTo + new Vector2(0, 20), TweenType.Linear, 15);
+            }
+            if (Input.KeyPress(Key.Left))
+            {
+                view.SetPosition(view.PositionGoTo + new Vector2(-20, 0), TweenType.Linear, 15);
+            }
+            if (Input.KeyPress(Key.Right))
+            {
+                view.SetPosition(view.PositionGoTo + new Vector2(20, 0), TweenType.Linear, 15);
+            }
+
+            view.Update();
+            Input.Update();
+        }
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            base.OnRenderFrame(e);
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+            GL.ClearColor(Color.CornflowerBlue);
+
+            Spritebatch.Begin(this.Width,this.Height);
+            view.ApplyTranform();
+
+            for (int i = -960; i < 960; i+=48) //земля
+            {
+                Spritebatch.Draw(textures[3], new Vector2(i, 0), new Vector2(3f, 3f), Color.Transparent, new Vector2(0, 0));
+                for (int j = 48; j < 480; j += 48) //земля
+                {
+                    Spritebatch.Draw(textures[2], new Vector2(i, j), new Vector2(3f, 3f), Color.Transparent, new Vector2(0, 0));
+                }
+            }
+            Spritebatch.Draw(textures[0], new Vector2(-96, 0), new Vector2(3f, 3f), Color.Transparent, new Vector2(0, 0));
+            Spritebatch.Draw(textures[0], new Vector2(240, 0), new Vector2(3f, 3f), Color.Transparent, new Vector2(0, 0));
+
+            for (int i = -48; i < 240; i += 48)
+            {
+                for (int j = -144; j < 0; j += 48)
+                {
+                    Spritebatch.Draw(textures[1], new Vector2(i, j), new Vector2(3f, 3f), Color.DarkGray, new Vector2(0, 0));
+                }
+            }
+            Spritebatch.Draw(textures[1], new Vector2(-96, -48), new Vector2(3f, 3f), Color.DarkGray, new Vector2(0, 0));
+            Spritebatch.Draw(textures[1], new Vector2(-96, -96), new Vector2(3f, 3f), Color.DarkGray, new Vector2(0, 0));
+            Spritebatch.Draw(textures[1], new Vector2(240, -48), new Vector2(3f, 3f), Color.DarkGray, new Vector2(0, 0));
+            Spritebatch.Draw(textures[1], new Vector2(240, -96), new Vector2(3f, 3f), Color.DarkGray, new Vector2(0, 0));
+
+            for (int i = -48; i < 240; i += 48)
+            {
+                Spritebatch.Draw(textures[1], new Vector2(i, 0), new Vector2(3f, 3f), Color.Transparent, new Vector2(0, 0));
+            }
+
+            for (int i = -96; i < 288; i += 48)
+            {
+                Spritebatch.Draw(textures[0], new Vector2(i, -192), new Vector2(3f, 3f), Color.Transparent, new Vector2(0, 0));
+            }
+            Spritebatch.Draw(textures[0], new Vector2(-96, -144), new Vector2(3f, 3f), Color.Transparent, new Vector2(0, 0));
+            Spritebatch.Draw(textures[0], new Vector2(240, -144), new Vector2(3f, 3f), Color.Transparent, new Vector2(0, 0));
+
+            SwapBuffers();
+        }
+    }
+}
---
+++ Game/View.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
+using System.Drawing;
+
+namespace Game
+{
+    public enum TweenType
+    {
+        Instant,
+        Linear,
+        QuadraticInOut,
+        CubicInOut,
+        QuarticOut
+    }
+    class View
+    {
+        private Vector2 position;
+
+        public double rotation;
+
+        public double zoom;
+
+        private Vector2 positionGoTo, posistionFrom;
+        private TweenType tweenType;
+        private int currentStep, tweenSteps;
+
+        public Vector2 Position
+        {
+            get
+            {
+                return this.position;
+            }
+        }
+
+        public Vector2 PositionGoTo
+        {
+            get
+            {
+                return this.positionGoTo;
+            }
+        }
+
+        public Vector2 ToWorld(Vector2 input)
+        {
+            input /= (float)zoom;
+            Vector2 dX = new Vector2((float)Math.Cos(rotation), (float)Math.Sin(rotation));
+            Vector2 dY = new Vector2((float)Math.Cos(rotation) + MathHelper.PiOver2, (float)Math.Sin(rotation) + MathHelper.PiOver2);
+
+            return (this.position + dX * input.X + dY * input.Y);
+        }
+
+        public View(Vector2 startPosition, double startRotation = 0.0, double startZoom = 1.0)
+        {
+            this.position = startPosition;
+            this.rotation = startRotation;
+            this.zoom = startZoom;
+        }
+
+        public void Update()
+        {
+            if (currentStep < tweenSteps)
+            {
+                switch (tweenType)
+                {
+                    case TweenType.Linear:
+                        position = posistionFrom + (positionGoTo - posistionFrom) * GetLinear((float)currentStep / tweenSteps);
+                        break;
+                    case TweenType.QuadraticInOut:
+                        position = posistionFrom + (positionGoTo - posistionFrom) * GetQuadraticInOut((float)currentStep / tweenSteps);
+                        break;
+                    case TweenType.CubicInOut:
+                        position = posistionFrom + (positionGoTo - posistionFrom) * GetCubicInOut((float)currentStep / tweenSteps);
+                        break;
+                    case TweenType.QuarticOut:
+                        position = posistionFrom + (positionGoTo - posistionFrom) * GetQuarticOut((float)currentStep / tweenSteps);
+                        break;
+                }
+
+                currentStep++;
+            }
+            else
+            {
+                position = positionGoTo;
+            }
+        }
+
+        public void SetPosition(Vector2 newPosition)
+        {
+            this.position = newPosition;
+            this.positionGoTo = newPosition;
+            this.posistionFrom = newPosition;
+            tweenSteps = 0;
+            currentStep = 0;
+            tweenType = TweenType.Instant;
+        }
+
+        public void SetPosition(Vector2 newPosition, TweenType type, int numSteps)
+        {
+            this.posistionFrom = position;
+            this.position = newPosition;
+            this.positionGoTo = newPosition;
+            tweenSteps = numSteps;
+            currentStep = 0;
+            tweenType = type;
+        }
+
+        public float GetLinear(float t)
+        {
+            return t;
+        }
+        public float GetQuadraticInOut(float t)
+        {
+            return (t * t) / ((2 * t * t) - (2 * t) + 1);
+        }
+        public float GetCubicInOut(float t)
+        {
+            return (t * t * t) / ((3 * t * t) - (3 * t) + 1);
+        }
+        public float GetQuarticOut(float t)
+        {
+            return -((t - 1) * (t - 1) * (t - 1) * (t - 1)) + 1;
+        }
+
+
+        public void ApplyTranform()
+        {
+            Matrix4 transform = Matrix4.Identity;
+
+            transform = Matrix4.Mult(transform,Matrix4.CreateTranslation(-position.X, -position.Y, 0));
+            transform = Matrix4.Mult(transform,Matrix4.CreateRotationZ(-(float)rotation));
+            transform = Matrix4.Mult(transform,Matrix4.CreateScale((float)zoom, (float)zoom, 1.0f));
+
+            GL.MultMatrix(ref transform);
+        }
+    }
+}
